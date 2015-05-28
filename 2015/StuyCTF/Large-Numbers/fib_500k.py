9b123e85dedb20a4515e41662a7f1446f57c8ed1
Dor-Ron <dor.rondel@gmail.com>
2015-05-28 18:20:00 -0400

    Create fib_500k.py

---
+++ 2015/StuyCTF/Large-Numbers/fib_500k.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+five_hundred_k = int(5 * 10e5)
+
+def fib(n):
+  a,b = 1,1
+  for i in range(n-1):
+    a,b = b,a+b
+  return a
+
+param = 0
+while True:
+    if len(str(fib(param))) == five_hundred_k:
+        large_num = fib(param)
+        break
+    else:
+        param += 1
+
+#print large_num
+large_num_sum = 0
+for num in str(fib(2392483))):
+    large_num_sum += int(num)
+
+print large_num_sum #=> 2250347
