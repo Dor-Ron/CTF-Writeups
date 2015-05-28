abfcf1e1848b20aef24ba5b8ab44c22593bba0dd
Dor-Ron <dor.rondel@gmail.com>
2015-05-27 21:09:02 -0400

    Create and_count.py

---
+++ 2015/StuyCTF/So-Many-Words/and_count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from urllib2 import urlopen
+
+words = urlopen("http://stuyctf.me/problem-static/So-Many-Words/dictionary.txt").read()
+list_of_words = words.split("\n")
+
+count = 0
+for word in list_of_words:
+    if "and" in word:
+        count += len(word)
+    else:
+        continue
+
+print "stuyctf{%d}" % count => stuyctf{38441}
