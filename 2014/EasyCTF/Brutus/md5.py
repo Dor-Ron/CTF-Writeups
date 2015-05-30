4a36429f285d4b726b7b0e92e6c6983b35fbeaa7
Dor-Ron <dor.rondel@gmail.com>
2015-05-30 00:35:00 -0400

    Create md5.py

---
+++ 2014/EasyCTF/Brutus/md5.py
@@ -0,0 +1,16 @@
+from hashlib import md5
+
+#got these from the text files. saves the open().read().split(" ") etc.
+numbers = [str(num) for num in xrange(1, 11)] #one-liners == <3
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'white', 'black']
+animals = ['cats', 'dog', 'mice', 'birds', 'fish', 'turtles', 'elephants', 'snakes', 'pigs', 'cows', 'goats']
+adjectives = ['cool', 'smart', 'funny', 'happy', 'weird', 'strange', 'normal', 'big', 'small', 'angry']
+
+string = ""
+for idx1 in numbers:
+    for idx2 in colors:
+        for idx3 in animals:
+            for idx4 in adjectives:
+                hash = md5(idx1 + idx2 + idx3 + idx4).hexdigest()
+                if hash == "f54f10fd6e38929084d505d0c2e9c997":
+                    print hash
